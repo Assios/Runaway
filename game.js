bdf8be83f1cb267373a40299a95965417ecbe4ea
Asbjørn Steinskog <asbjorn@steinskog.me>
2014-05-11 16:23:05 +0200

    Modify distance

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ var w = 800;
 var h = 300;
 var SPEED = 400;
 var STONESPEED = 420;
-var distance = 10000;
+var distance = 1000;
 
 var game = new Phaser.Game(800, 300, Phaser.AUTO, '', null, false, false);
 
@@ -11,4 +11,3 @@ game.state.add('menu', menu);
 game.state.add('play', play);
 
 game.state.start('load');
-
